5f5d2fa274864e34704a236c43976aa5069b9062
nexlice <auspiciouswho47@gmail.com>
2022-04-10 15:49:48 +0900

    made data package

--- Dopt_app/app/src/main/java/com/example/dopt_app/MainActivity.kt
+++ Dopt_app/app/src/main/java/com/example/dopt_app/MainActivity.kt
@@ -12,10 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         initBottomNavigation()
-
-        
     }
 
     private fun initBottomNavigation(){
