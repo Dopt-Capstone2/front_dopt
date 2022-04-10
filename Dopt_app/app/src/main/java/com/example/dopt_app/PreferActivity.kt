3bb18eb358b6f2863ae1d0ff4ff5b0b2d98e08d8
Chaehy <chlee7670@cau.ac.kr>
2022-04-10 18:02:02 +0900

    style: add popup style

--- Dopt_app/app/src/main/java/com/example/dopt_app/PreferActivity.kt
+++ Dopt_app/app/src/main/java/com/example/dopt_app/PreferActivity.kt
@@ -1,12 +1,20 @@
 package com.example.dopt_app
 
+import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
+import com.example.dopt_app.databinding.ActivityPreferBinding
+import kotlinx.android.synthetic.main.activity_nickname.*
 
 class PreferActivity : AppCompatActivity() {
+    lateinit var binding: ActivityPreferBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_prefer)
+
 
     }
 }
