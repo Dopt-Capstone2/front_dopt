6ee1b20200397dbfaaf628234ef30c625af33f23
Chaehy <chlee7670@cau.ac.kr>
2022-05-01 22:54:15 +0900

    data: add animal data class

---
+++ Dopt_app/app/src/main/java/com/example/dopt_app/Animal.kt
@@ -0,0 +1,29 @@
+package com.example.dopt_app
+
+import java.util.*
+
+data class Animal (
+    var age: Int? = null,
+    var careAddr: String?= "",
+    var careNm: String?="",
+    var careTel: String?="",
+    var chargeNm: String?="",
+    var colorCd: String?="",
+    var desertionNo: Int?=null,
+    var filename: String?="",
+    var happenDt: Int?=null,
+    var happenPlace: String?="",
+    var kindCd: String?="",
+    var neuterYn: String?="",
+    var noticeEdt: Int?=null,
+    var noticeNo: String?="",
+    var noticeSdt: Int?=null,
+    var officetel: Int?=null,
+    var orgNm: String?="",
+    var popfile: String?="",
+    var processState: String?="",
+    var sexCd: String?="",
+    var specialMark: String?="",
+    var weight: String?="",
+    )
+
