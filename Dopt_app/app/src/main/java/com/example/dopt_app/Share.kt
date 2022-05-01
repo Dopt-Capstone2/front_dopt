d5483078ea61ff512ad4bf24586faeb5a03dc991
Chaehy <chlee7670@cau.ac.kr>
2022-05-01 23:42:54 +0900

    style: add share item

---
+++ Dopt_app/app/src/main/java/com/example/dopt_app/Share.kt
@@ -0,0 +1,9 @@
+package com.example.dopt_app
+
+data class Share(
+    var title: String?="",
+    var text: String?="",
+    var aniImg: Int? = null,
+    var upDate: String = "",
+    var userId: String?="",
+)
