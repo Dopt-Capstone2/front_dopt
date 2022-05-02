ba3b02ab95211ff8e9418df5d237029efd8be5da
nexlice <auspiciouswho47@gmail.com>
2022-05-02 21:53:04 +0900

    getting api successed in log

--- Dopt_app/app/src/main/java/com/example/dopt_app/data/AnimalOpenAPI.kt
+++ Dopt_app/app/src/main/java/com/example/dopt_app/data/AnimalOpenAPI.kt
@@ -5,21 +5,38 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Url
 
 //참고 블로그
-//https://kumgo1d.tistory.com/57
+//전체적인 구현에 대한 설명
+//TODO: https://kumgo1d.tistory.com/57
+//레트로핏 인자에 대한 자세한 설명
+//TODO: https://jaejong.tistory.com/33
 //주소에 접근하기 위한 인터페이스 생성
 class AnimalOpenAPI {
     companion object {
         //xml 형식으로 보여주는 "&type=xml" 은 도메인에서 제거
-        const val DOMAIN = "http://openapi.animal.go.kr:80/openapi/service/rest/abandonmentPublicSrvc?_wadl"
-        const val API_KEY = "SSp25i3dc5GkEAwqr6qrKHLAPS7aMZ/aKuVyMlE+qQ1irBnGaQNkbmm24XJF05S42SXMwQIIcIeC+vm6IggUOQ=="
+        //주소가 '/'로 끝나지 않으면 터진다..
+        //https://cishome.tistory.com/235?category=900738
+        //그리고 주소에는 ?를 포함시키면 안된다.
+        //https://stackoverflow.com/questions/38395460/java-lang-illegalargumentexception-baseurl-must-end-in-while-using-retrofit-2
+        //okhttp 에러가 난다면 gradle에서 okhttp버전을 업데이트해준다.
+        //https://stackoverflow.com/questions/56818660/how-to-fix-expected-android-api-level-21-but-was-19-in-android
+        //도메인은 공공데이터 문서에 나와있는 rest 개발환경 서비스 url을 사용
+        const val DOMAIN = "http://openapi.animal.go.kr/openapi/service/rest/abandonmentPublicSrvc/abandonmentPublic/"
+        const val API_KEY = "ServiceKey=SSp25i3dc5GkEAwqr6qrKHLAPS7aMZ%2FaKuVyMlE%2BqQ1irBnGaQNkbmm24XJF05S42SXMwQIIcIeC%2Bvm6IggUOQ%3D%3D"
     }
     interface AnimalOpenService{
-        //TODO: 이 부분 수정이 필요함
-        @GET("{api_key}/json")
-        //TODO: call할 객체를 수정할 수도 있음.
-        fun getAnimal(@Path("api_key") key: String) : Call<Items>
+        //무슨 이유에선지 잘 안된다..
+        //@GET("sido?{api_key}&_type=json")
+        //fun getAnimal(@Path("api_key") key: String) : Call<OpenAnimal>
+        
+        //dynamic url로 구현
+        //TODO: 실행시에 한 번, 실시간으로 입양이 된다면 동물이 카드에서 사라져야하므로 실시간으로도 수행되어야함
+        //TODO: 현재는 비동기적으로 동작.
+        //참고 블로그
+        //https://realapril.tistory.com/54
+        @GET fun getAnimal(@Url url:String) : Call<OpenAnimal>
     }
 
     //오브젝트 클래스
