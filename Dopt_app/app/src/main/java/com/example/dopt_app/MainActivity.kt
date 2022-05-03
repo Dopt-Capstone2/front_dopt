02fb8a7ca7a10b8e258e6f1488a1ecb0787957de
nexlice <auspiciouswho47@gmail.com>
2022-05-03 11:25:46 +0900

    wrote todo

--- Dopt_app/app/src/main/java/com/example/dopt_app/MainActivity.kt
+++ Dopt_app/app/src/main/java/com/example/dopt_app/MainActivity.kt
@@ -46,6 +46,10 @@ class MainActivity : AppCompatActivity() {
     private fun getAnimalData(): MutableLiveData<OpenAnimal> {
         val call = AnimalOpenAPI.AnimalRetrofitClient.animalOpenService
 
+        //TODO: 동물을 현재일로부터 10일 전부터 조회할 것. ex) 오늘이 11일이라면 1일부터 조회.
+        //TODO: 서비스키 뒤로 현재 일수를 받아 noticeSdt에 넣어주면 됨.
+        //TODO: notiveSdt의 format: 20220503 (Int)
+        //이전의 bgnde는 더이상 사용하지 않습니다.
         call.getAnimal("http://openapi.animal.go.kr/openapi/service/rest/abandonmentPublicSrvc/abandonmentPublic?ServiceKey=SSp25i3dc5GkEAwqr6qrKHLAPS7aMZ%2FaKuVyMlE%2BqQ1irBnGaQNkbmm24XJF05S42SXMwQIIcIeC%2Bvm6IggUOQ%3D%3D&_type=json").enqueue(object: Callback<OpenAnimal>{
             override fun onResponse(call: Call<OpenAnimal>, response: Response<OpenAnimal>){
                 animalResponse.value = response.body() as OpenAnimal
