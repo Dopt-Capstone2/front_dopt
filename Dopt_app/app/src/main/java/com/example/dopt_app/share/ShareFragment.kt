96383dc4dfdff5b59251ed8fdae84ba710e1747b
Chaehy <chlee7670@cau.ac.kr>
2022-05-03 18:18:00 +0900

    feat: add my share recycler view

--- Dopt_app/app/src/main/java/com/example/dopt_app/share/ShareFragment.kt
+++ Dopt_app/app/src/main/java/com/example/dopt_app/share/ShareFragment.kt
@@ -5,11 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.dopt_app.MainActivity
+import com.example.dopt_app.R
+import com.example.dopt_app.data.Share
 import com.example.dopt_app.databinding.FragmentShareBinding
+import com.google.gson.Gson
 
 class ShareFragment : Fragment() {
 
     lateinit var binding: FragmentShareBinding
+    private var myshareDatas = ArrayList<Share>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,6 +25,56 @@ class ShareFragment : Fragment() {
     ): View? {
         binding = FragmentShareBinding.inflate(inflater, container, false)
 
+        myshareDatas.apply {
+            add(
+                Share("멍멍이의 성장과정", "입양했을때는 엄청 작았는데 벌써 이렇게 컸답니다ㅠㅠ\n 정말 뿌듯해요\n" +
+                    " 다음 주차에도 후기 올리러 오겠습니다 ;) \n" +
+                    " 만관부~~", R.drawable.dog_num_one, "2022-05-02", "test11", "채현채현", "1주차")
+            )
+            add(
+                Share("멍멍이의 성장과정", "입양했을때는 엄청 작았는데 벌써 이렇게 컸답니다ㅠㅠ\n 정말 뿌듯해요 왕왕\n" +
+                    " 다음 주차에도 후기 올리러 오겠습니다 ;) \n" +
+                    " 만관부~~", R.drawable.dog_num_one, "2022-05-02", "test11", "채현채현", "2주차")
+            )
+            add(
+                Share("냥냥이의 성장과정", "입양했을때는 엄청 작았는데 벌써 이렇게 컸답니다ㅠㅠ\n 정말 뿌듯해요\n" +
+                    " 다음 주차에도 후기 올리러 오겠습니다 ;) \n" +
+                    " 만관부~~", R.drawable.cat_num_one, "2022-05-02", "test11", "채현채현", "3주차")
+            )
+            add(
+                Share("멍멍이의 성장과정", "입양했을때는 엄청 작았는데 벌써 이렇게 컸답니다ㅠㅠ\n 정말 뿌듯해요\n" +
+                    " 다음 주차에도 후기 올리러 오겠습니다 ;) \n" +
+                    " 만관부~~", R.drawable.dog_num_one, "2022-05-02", "test11", "채현채현", "4주차")
+            )
+        }
+
+        // 더미데이터랑 Adapter 연결
+        val myshareRVAdapter = MyShareRVAdapter(myshareDatas)
+        // 리사이클러뷰에 어댑터를 연결
+        binding.shareMyAnimalsRv.adapter = myshareRVAdapter
+
+        myshareRVAdapter.setMyItemClickListener(object : MyShareRVAdapter.MyItemClickListener{
+
+            override fun onItemClick(share: Share) {
+                changeMyshareFragment(share)
+            }
+
+        })
+        // 레이아웃 매니저 설정
+        binding.shareMyAnimalsRv.layoutManager = GridLayoutManager(context, 2, )
+
         return binding.root
+    }
+
+    private fun changeMyshareFragment(share: Share) {
+        (context as MainActivity).supportFragmentManager.beginTransaction()
+            .replace(R.id.main_frm, MyShareFragment().apply {
+                arguments = Bundle().apply {
+                    val gson = Gson()
+                    val myshareJson = gson.toJson(share)
+                    putString("share", myshareJson)
+                }
+            })
+            .commitAllowingStateLoss()
     }
 }
