ec3224fb72fbadd335981aae5abf99f28c50c30b
icecream126 <kim123hyomin@gmail.com>
2022-05-10 23:51:55 +0900

    feat: save Useremail, Userpw at db

--- Dopt_app/app/src/main/java/com/example/dopt_app/api/RetrofitClient.kt
+++ Dopt_app/app/src/main/java/com/example/dopt_app/api/RetrofitClient.kt
@@ -1,38 +1,32 @@
 package com.example.dopt_app.api
 
-import android.util.Base64.*
-import android.util.Base64
 import okhttp3.OkHttpClient
-import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.util.*
 
+import retrofit2.Retrofit
+
+// Retrofit Client 참고
+//https://stackoverflow.com/questions/65699941/retrofit-post-on-android-kotlin-not-working-but-working-on-postman
 
 object RetrofitClient {
-
-    private val AUTH = "Basic "+Base64.encodeToString("admin:123456".toByteArray(), Base64.DEFAULT)
-
-    private const val BASE_URL="ec2-54-241-117-48.us-west-1.compute.amazonaws.com"
-
+    private const val BASE_URL = "http://ec2-54-241-117-48.us-west-1.compute.amazonaws.com:3000"
     private val okHttpClient = OkHttpClient.Builder()
-            .addInterceptor{ chain ->
-                val original=chain.request()
+        .addInterceptor {chain ->
+            val original = chain.request()
 
-                val requestBuilder=original.newBuilder()
-                    .addHeader("Authorization", "")
-                    .method(original.method(), original.body())
+            val requestBuilder = original.newBuilder()
+                .method(original.method(), original.body())
 
-                val request = requestBuilder.build()
-                chain.proceed(request)
-            }.build()
+            val request = requestBuilder.build()
+            chain.proceed(request)
+        }.build()
 
-    val instance: JoinAPI by lazy{
+    val instance: JoinAPI by lazy {
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .client(okHttpClient)
             .build()
         retrofit.create(JoinAPI::class.java)
-
     }
 }
--- Dopt_app/app/src/main/java/com/example/dopt_app/auth/JoinActivity.kt
+++ Dopt_app/app/src/main/java/com/example/dopt_app/auth/JoinActivity.kt
@@ -1,13 +1,18 @@
 package com.example.dopt_app.auth
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
-import com.example.dopt_app.NicknameActivity
+import android.widget.Toast
 import com.example.dopt_app.R
+import com.example.dopt_app.api.RetrofitClient
+import com.example.dopt_app.data.Join
 import com.google.android.material.textfield.TextInputEditText
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 
 class JoinActivity : AppCompatActivity() {
 
@@ -19,17 +24,30 @@ class JoinActivity : AppCompatActivity() {
 
         val joinBtn = findViewById<Button>(R.id.joinBtn)
         joinBtn.setOnClickListener{
-            val email= findViewById<TextInputEditText>(R.id.join_email_jo)
-            val pwd = findViewById<TextInputEditText>(R.id.join_pw_jo)
-            val intent= Intent(this, NicknameActivity::class.java)
+            val userEmail= findViewById<TextInputEditText>(R.id.join_email_jo)
+            val userPw = findViewById<TextInputEditText>(R.id.join_pw_jo)
+            //val intent= Intent(this, NicknameActivity::class.java)
             startActivity(intent)
 
-            Log.d(TAG, email.text.toString())
-            Log.d(TAG, pwd.text.toString())
+            RetrofitClient.instance.userJoin(userEmail,userPw)
+                .enqueue(object: Callback<Join> {
+                    override fun onFailure(call: Call<Join>, t: Throwable) {
+                        Log.d(TAG, "Request Failed")
+                        Toast.makeText(applicationContext,t.message, Toast.LENGTH_LONG).show()
+                        Log.d(TAG, "Request Failed end")
+                    }
+                    override fun onResponse(call: Call<Join>, response: Response<Join>) {
+                        Log.d(TAG,"Request successful")
+                        Log.d(TAG, response.body().toString())
+                        Toast.makeText(applicationContext, response.body().toString(), Toast.LENGTH_LONG).show()
+                    }
+                }
+                )
+
+            Log.d(TAG, "auth/JoinActivity")
+            Log.d(TAG, userEmail.text.toString())
+            Log.d(TAG, userPw.text.toString())
 
         }
-
-
-
     }
 }
