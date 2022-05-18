4abba5c35c00d3e55cbc019c36a105e3ec29f0de
nexlice <auspiciouswho47@gmail.com>
2022-05-18 23:33:26 +0900

    disabled api

--- Dopt_app/app/src/main/java/com/example/dopt_app/auth/JoinActivity.kt
+++ Dopt_app/app/src/main/java/com/example/dopt_app/auth/JoinActivity.kt
@@ -34,7 +34,7 @@ class JoinActivity : AppCompatActivity() {
 //            val userEmail= findViewById<TextInputEditText>(R.id.join_email_jo)
 //            val userPw = findViewById<TextInputEditText>(R.id.join_pw_jo)
 //            val data = User_Signup(userEmail.text.toString(), userPw.text.toString(), "hmin","Ilsan","nick")
-//            startActivity(intent)
+            startActivity(intent)
 //            Log.d(TAG, "clicked join btn!!!")
 //            Log.d(TAG, data.toString())
 //            RetrofitClient.User_Signup_instance.POST_User_SignUp(data)
@@ -74,43 +74,43 @@ class JoinActivity : AppCompatActivity() {
 //                }
 //                )
 
-            //북마크 정보 불러오기
-            //북마크들의 배열을 호출한다
-            //파라미터로 사용자의 이메일을 입력한다
-
-            //데이터 클래스로 저장은 아래처럼 합니다.
-            //정보 저장 객체
-            var bookmarkResponse = MutableLiveData<Bookmark_List>()
-            RetrofitClient.Bookmark_instance.GET_Bookmark("123@123")
-                .enqueue(object: Callback <Bookmark_List> {
-                    override fun onFailure(call: Call<Bookmark_List>, t: Throwable) {
-                        Toast.makeText(applicationContext,t.message, Toast.LENGTH_LONG).show()
-                        Log.d(TAG, "failed")
-                        Log.d(TAG, t.message.toString())
-                    }
-                    override fun onResponse(call: Call<Bookmark_List>, response: Response<Bookmark_List>) {
-                        Toast.makeText(
-                            applicationContext,
-                            response.body().toString(),
-                            Toast.LENGTH_LONG
-                        ).show()
-                        Log.d(TAG, "succeeded")
-                        Log.d(TAG, response.body().toString())
-
-                        //지정한 데이터 클래스 객체로 저장
-                        bookmarkResponse.value = response.body() as Bookmark_List
-                        // 값을 복사
-                        val bookmarkRaw = bookmarkResponse.value?.copy()
-                        // 데이터 클래스들의 배열 출력
-                        Log.d("bookmarkRaw", bookmarkRaw.toString())
-                        //요소별 접근
-                        //response의 각 데이터 클래스 접근
-                        if (bookmarkRaw != null) {
-                            Log.d("bookmark", bookmarkRaw.Bookmark[0].toString())
-                        }
-                    }
-                }
-                )
+//            //북마크 정보 불러오기
+//            //북마크들의 배열을 호출한다
+//            //파라미터로 사용자의 이메일을 입력한다
+//
+//            //데이터 클래스로 저장은 아래처럼 합니다.
+//            //정보 저장 객체
+//            var bookmarkResponse = MutableLiveData<Bookmark_List>()
+//            RetrofitClient.Bookmark_instance.GET_Bookmark("123@123")
+//                .enqueue(object: Callback <Bookmark_List> {
+//                    override fun onFailure(call: Call<Bookmark_List>, t: Throwable) {
+//                        Toast.makeText(applicationContext,t.message, Toast.LENGTH_LONG).show()
+//                        Log.d(TAG, "failed")
+//                        Log.d(TAG, t.message.toString())
+//                    }
+//                    override fun onResponse(call: Call<Bookmark_List>, response: Response<Bookmark_List>) {
+//                        Toast.makeText(
+//                            applicationContext,
+//                            response.body().toString(),
+//                            Toast.LENGTH_LONG
+//                        ).show()
+//                        Log.d(TAG, "succeeded")
+//                        Log.d(TAG, response.body().toString())
+//
+//                        //지정한 데이터 클래스 객체로 저장
+//                        bookmarkResponse.value = response.body() as Bookmark_List
+//                        // 값을 복사
+//                        val bookmarkRaw = bookmarkResponse.value?.copy()
+//                        // 데이터 클래스들의 배열 출력
+//                        Log.d("bookmarkRaw", bookmarkRaw.toString())
+//                        //요소별 접근
+//                        //response의 각 데이터 클래스 접근
+//                        if (bookmarkRaw != null) {
+//                            Log.d("bookmark", bookmarkRaw.Bookmark[0].toString())
+//                        }
+//                    }
+//                }
+//                )
         }
     }
 }
