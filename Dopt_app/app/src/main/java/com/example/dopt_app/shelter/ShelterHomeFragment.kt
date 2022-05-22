0882c8648248b80aa2229eab32a77646e7dc89db
Chaehy <chlee7670@cau.ac.kr>
2022-05-22 23:23:50 +0900

    feat: shelter fragment

--- Dopt_app/app/src/main/java/com/example/dopt_app/shelter/ShelterHomeFragment.kt
+++ Dopt_app/app/src/main/java/com/example/dopt_app/shelter/ShelterHomeFragment.kt
@@ -1,6 +1,24 @@
 package com.example.dopt_app.shelter
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.dopt_app.databinding.FragmentShelterHomeBinding
 
-class ShelterHomeFragment: Fragment() {
+class ShelterHomeFragment : Fragment() {
+
+    lateinit var binding: FragmentShelterHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentShelterHomeBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
 }
--- Dopt_app/app/src/main/java/com/example/dopt_app/shelter/ShelterShareFragment.kt
+++ Dopt_app/app/src/main/java/com/example/dopt_app/shelter/ShelterShareFragment.kt
@@ -1,6 +1,24 @@
 package com.example.dopt_app.shelter
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.dopt_app.databinding.FragmentShelterShareBinding
 
-class ShelterShareFragment: Fragment() {
+class ShelterShareFragment : Fragment() {
+
+    lateinit var binding: FragmentShelterShareBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentShelterShareBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
 }
