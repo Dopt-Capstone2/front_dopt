023ebf080519585d91c6153e5d070dace9b1c842
nexlice <auspiciouswho47@gmail.com>
2022-05-23 16:21:59 +0900

    changed preference dataclass

--- Dopt_app/app/src/main/java/com/example/dopt_app/data/Preference.kt
+++ Dopt_app/app/src/main/java/com/example/dopt_app/data/Preference.kt
@@ -2,6 +2,7 @@ package com.example.dopt_app.data
 
 //TODO: (trivial) 현재 선호를 하나만 작성할 수 있음
 data class Preference (
+    val number: Int,
     val userEmail: String,
     val breed: String,
     val age: Int,
