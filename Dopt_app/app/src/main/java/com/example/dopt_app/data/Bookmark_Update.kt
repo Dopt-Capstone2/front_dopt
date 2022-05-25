4cbedf829a7875261193c0b774d87bc6da7040bc
nexlice <auspiciouswho47@gmail.com>
2022-05-25 18:53:11 +0900

    shelter api done

---
+++ Dopt_app/app/src/main/java/com/example/dopt_app/data/Bookmark_Update.kt
@@ -0,0 +1,8 @@
+package com.example.dopt_app.data
+
+//shelter의 isConsidred UPDATE를 위한 데이터 클래스
+data class Bookmark_Update (
+    val careNm: String,
+    val desertionNo: String,
+    val isConsidered: Int
+)
