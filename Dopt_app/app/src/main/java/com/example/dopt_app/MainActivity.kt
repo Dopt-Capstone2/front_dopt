44e696d13f766647e057efcb610f26314118e6be
Chaehy <chlee7670@cau.ac.kr>
2022-06-03 02:15:47 +0900

    feat: main

--- Dopt_app/app/src/main/java/com/example/dopt_app/MainActivity.kt
+++ Dopt_app/app/src/main/java/com/example/dopt_app/MainActivity.kt
@@ -9,6 +9,8 @@ import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import com.example.dopt_app.account.AccountFragment
 import com.example.dopt_app.api.RetrofitClient
+import com.example.dopt_app.auth.*
+import com.example.dopt_app.data.Item
 import com.example.dopt_app.data.Match
 import com.example.dopt_app.data.Preference
 import com.example.dopt_app.databinding.ActivityMainBinding
@@ -24,7 +26,10 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-// import java.time.LocalDate
+val data = Preference("default1", "123@123", "개", "1", "M","흰색","개")
+//data부분에 인자 넘기기 여기서 data는 preferece 데이터 클래스 객체
+var Match_Response = MutableLiveData<Match>()
+lateinit var matchDatas : Match
 
 class MainActivity : AppCompatActivity() {
 
@@ -107,58 +112,9 @@ class MainActivity : AppCompatActivity() {
             when (item.itemId) {
 
                 R.id.homeFragment -> {
-//                    //Log.d("date-1", bgnde_Monthly)
-//                    val animalRaw_Monthly = animalResponse_10Days.value?.copy()
-//                    //속도차이 때문에 바로 뜨지 않음.
-//                    val animalItems_Monthly = animalRaw_Monthly?.response?.body?.items
-//                    var mercysum = 0
-//                    var rescuesum =0
-//                    if (animalItems_Monthly != null) {
-//                        println(animalItems_Monthly.item.size)
-//                        //println(animalItems_Monthly.item[0])
-//                        //val mercy = {p : Item -> p.processState == "종료(안락사)"}
-//                        //println(animalItems_Monthly.item.count(mercy))
-//                        //Log.d("item[0] processState", animalItems_Monthly.item[0].processState)
-//                        for (i in animalItems_Monthly.item)
-//                            if (i.processState=="종료(안락사)") {
-//                                mercysum += 1
-//                                continue
-//                            }
-//                            else if (i.processState=="종료(입양)") {
-//                                rescuesum += 1
-//                                continue
-//                            }
-//                        println(mercysum)
-//                        println(rescuesum)
-//
-//                    } else{
-//                        println("animal_Monthly is null")
-//                    }
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.main_frm, HomeFragment())
-                        .commitAllowingStateLoss()
-                    return@setOnItemSelectedListener true
-                }
-
-                R.id.starFragment -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.main_frm, StarFragment())
-                        .commitAllowingStateLoss()
-                    return@setOnItemSelectedListener true
-                }
-                /*R.id.matchFragment -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.main_frm, MatchFragment())
-                        .commitAllowingStateLoss()
-                    return@setOnItemSelectedListener true
-                }*/
-                R.id.matchFragment -> {
                     Log.d(TAG, "Enter Match Fragment")
                     //get 한 preference 혹은 글로벌 변수로 저장한 preference를 인자로 넘겨주세요
                     //더미 데이터
-                    val data = Preference("default1", "123@123", "개", "1", "M","흰색","개")
-                    //data부분에 인자 넘기기 여기서 data는 preferece 데이터 클래스 객체
-                    var Match_Response = MutableLiveData<Match>()
                     //RetrofitClient.Match_instance.GET_Match(data)
                     //파라미터로 전하는 데이터 형식은 다음과 같습니다.
                     //preference의 모든 값,
@@ -166,7 +122,7 @@ class MainActivity : AppCompatActivity() {
                     //여기서는 파라미터를 데이터클래스 객체로 전하지 않고,
                     //모두 분해서 하나씩 전합니다.
                     //deconstructor 등으로 파라미터를 전달해주세요.
-                    RetrofitClient.Match_instance.GET_Match("default1","123@123","개", "1", "M", "흰색", "개", "서울특별시")
+                    RetrofitClient.Match_instance.GET_Match(emailInfo,emailInfo,preferKind, preferAge, preferGender, preferColor, preferBreed, "서울특별시")
                         .enqueue(object: Callback<Match> {
                             override fun onFailure(call: Call<Match>, t: Throwable) {
                                 Toast.makeText(applicationContext, t.message, Toast.LENGTH_LONG)
@@ -186,6 +142,9 @@ class MainActivity : AppCompatActivity() {
                                 Match_Response.value = response.body() as Match
                                 // 값을 복사
                                 val Match_Raw = Match_Response.value?.copy()
+
+                                matchDatas= Match_Response.value!!.copy()
+
                                 // 데이터 클래스들의 배열 출력
                                 Log.d("Match_Raw", Match_Raw.toString())
                                 //요소별 접근
@@ -194,11 +153,29 @@ class MainActivity : AppCompatActivity() {
                                     Log.d("MatchItems", Match_Raw.data[0].toString())
                                 }
                             }
-                        }
-                        )
+                        })
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, HomeFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
 
+                R.id.starFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, StarFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                /*R.id.matchFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, MatchFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }*/
+                R.id.matchFragment -> {
 
                     val intent= Intent(this, MatchActivity::class.java)
+
                     startActivity(intent)
                     // return@setOnItemSelectedListener true
                 }
